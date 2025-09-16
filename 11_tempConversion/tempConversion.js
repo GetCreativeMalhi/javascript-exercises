39cd153b71e4f7db4526f3a8e431e3bb1c1b4b33
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-17 09:36:01 +1000

    feat: add convertToCelsius and convertToFahrenheit
    - convertToCelsius function converts Fahrenheit
     to Celsius
    - convertToFahrenheit function converts Celsius to
    Fahrenheit
    - both functions return temp rounded to 1 decimal
    place

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+   let num = ((tempFahrenheit - 32) * 5 / 9);
+   return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let num = ((tempCelsius * (9/5))+ 32);
+  return Math.round(num * 10) / 10;
 };
-
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
