c41c35c93bbf8e2117c0683c2939ccabbd4d734d
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-16 12:47:08 +1000

    feat: add string repeat function
    - this function repeats a string without using
    the string built in repeat function and it also
    handles negative number error.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
 
+const repeatString = function(word, num) {
+    let string1 = "";
+    if (num < 0){
+        return "ERROR";
+    } 
+    for(let i = 1; i <= num; i++){
+       
+        string1 += word ;  
+        
+    } 
+    
+    return  string1;
 };
 
 // Do not edit below this line
