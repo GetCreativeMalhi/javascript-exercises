2a854f6c3c3160b211f8dc52b49a37f1637cd74b
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-09-16 15:45:02 +1000

    feat: add sumAll function
    - takes two numbers as arguments
    - calculates sum of all the numbers in-between
    (including)
    - returns "ERROR" if its not number, negative
    numbers or decimals.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof(num1)!= "number") || (typeof(num2)!= "number") || num1 < 0 || num2 < 0 || num1 % 1 !== 0 || num2 % 1 !== 0){
+        return "ERROR";
+    } else { 
+        let big;
+        let small;
+        if(num1 > num2){
+            big = num1;
+            small = num2;
+            
+        }else {
+            big = num2;
+            small = num1;
+           
+        } 
+        let sum = 0;
 
+        while(small <= big){
+        sum += small;
+        small++;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
