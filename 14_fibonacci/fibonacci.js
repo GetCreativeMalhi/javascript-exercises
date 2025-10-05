d83540f29ef38fce59a5cb6cb1ab7ace8a970d6b
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-10-05 17:57:49 +1100

    feat: add fibonacci function
    -it retrieves nth place fibonacci number
    -handles string input by converting it to number
    -returns "OOPS" for -ve numbers

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let i = 2;
+    num = +num;
+    let fibonacciSum = 0;
+    let firstNum = 0;
+    let secondNum = 1;
+    let arr =[0, 1];
+    if(num < 0){
+        return "OOPS";
+    }
+    while(i <= num) {
+        fibonacciSum = firstNum + secondNum;
+        arr.push(fibonacciSum);
+        firstNum = secondNum;
+        secondNum = fibonacciSum;
+        i++;
+    }
+    return arr[num];
 };
 
 // Do not edit below this line
