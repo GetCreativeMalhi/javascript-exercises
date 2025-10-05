9351b26a596e9e6b81ef148f4e2fcbcd5aeed208
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-10-05 13:35:52 +1100

    feat: add palindromes function and test
    -add palindromes function reads the same backwards
    as forwards
    -use regular expression to filter non alphabets
    before comparison

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr= str.toLowerCase().replace(/\W/g, "");
+    const arr = newStr.split("");
+    const comparisonStr = arr.join("");
+    const comparisonStrReverse = arr.reverse().join("");
+    return comparisonStr === comparisonStrReverse;
 };
 
 // Do not edit below this line
