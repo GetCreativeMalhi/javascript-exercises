3b3c56122ac424bbdb8b8120e90d893107871ab1
GetCreativeMalhi <108103415+GetCreativeMalhi@users.noreply.github.com>
2025-10-06 11:36:35 +1100

    feat: add findTheOldest function
    -find the oldest person out of array of people
    objects
    -if no year of death provided uses current year to
    calculate age

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const date = new Date();
+    let oldest = arr.reduce((previousValue, currentValue) => {
+        let previousYearOfDeath = previousValue.yearOfDeath || date.getFullYear();
+        let currentYearOfDeath = currentValue.yearOfDeath || date.getFullYear();
+        if((previousYearOfDeath - previousValue.yearOfBirth) > (currentYearOfDeath - currentValue.yearOfBirth)){
+            return previousValue;
+        }else{
+            return currentValue;
+        }
+    });
+   return oldest;
 };
 
 // Do not edit below this line
